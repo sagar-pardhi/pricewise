bb92819f4fe76604a45d9c2e03ba02dc9dcc705d
sagar-pardhi <sgrpardhi007@gmail.com>
2024-01-13 11:31:45 +0530

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default async function Home() {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {allProducts.map((product) => (
+          {allProducts?.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
         </div>
